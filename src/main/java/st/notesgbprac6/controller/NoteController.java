c37c70ea43e9b3e6ba642f67066c68e50b92c4c3
StephYa7 <1000p@mail.ru>
2024-02-15 00:57:29 +0700

    added all methods in NoteController

--- src/main/java/st/notesgbprac6/controller/NoteController.java
+++ src/main/java/st/notesgbprac6/controller/NoteController.java
@@ -1,11 +1,57 @@
 package st.notesgbprac6.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import st.notesgbprac6.model.Note;
 import st.notesgbprac6.service.NoteService;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/notes")
 public class NoteController {
     NoteService noteService;
+
+    @PostMapping
+    public ResponseEntity<Note> saveNote(@RequestBody Note note) {
+        Note newNote = noteService.saveNote(note);
+        if (newNote == null) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok(newNote);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Note>> findAllNotes() {
+        List<Note> notes = noteService.findAllNotes();
+        if (notes.isEmpty()) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(notes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+        Note note = noteService.getNoteById(id);
+        if (note == null) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(note);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateNote(@PathVariable Long id, @RequestBody Note note) {
+        Note updateNote = noteService.updateNote(id, note);
+        if (updateNote == null) ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
+        Note deleteNote = noteService.deleteNoteById(id);
+        if (deleteNote == null) ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/st/notesgbprac6/service/NoteService.java
+++ src/main/java/st/notesgbprac6/service/NoteService.java
@@ -2,10 +2,28 @@ package st.notesgbprac6.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import st.notesgbprac6.model.Note;
 import st.notesgbprac6.repository.NoteRepository;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class NoteService {
     NoteRepository noteRepository;
+
+    public Note saveNote(Note note) {
+    }
+
+    public List<Note> findAllNotes() {
+    }
+
+    public Note getNoteById(Long id) {
+    }
+
+    public Note updateNote(Long id, Note note) {
+    }
+
+    public Note deleteNoteById(Long id) {
+    }
 }
