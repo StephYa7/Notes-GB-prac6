19e57d3bcb917c9b247331e0f97163fa4338b4a1
StephYa7 <1000p@mail.ru>
2024-02-15 01:57:55 +0700

    added all method and fix after test

--- src/main/java/st/notesgbprac6/controller/NoteController.java
+++ src/main/java/st/notesgbprac6/controller/NoteController.java
@@ -2,14 +2,14 @@ package st.notesgbprac6.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import st.notesgbprac6.model.Note;
 import st.notesgbprac6.service.NoteService;
 
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/notes")
 public class NoteController {
@@ -17,7 +17,9 @@ public class NoteController {
 
     @PostMapping
     public ResponseEntity<Note> saveNote(@RequestBody Note note) {
+
         Note newNote = noteService.saveNote(note);
+
         if (newNote == null) return ResponseEntity.badRequest().build();
 
         return ResponseEntity.ok(newNote);
@@ -33,16 +35,16 @@ public class NoteController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
-        Note note = noteService.getNoteById(id);
-        if (note == null) return ResponseEntity.notFound().build();
+        Optional<Note> note = noteService.getNoteById(id);
 
-        return ResponseEntity.ok(note);
+        return note.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateNote(@PathVariable Long id, @RequestBody Note note) {
         Note updateNote = noteService.updateNote(id, note);
-        if (updateNote == null) ResponseEntity.notFound().build();
+        if (updateNote == null) return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok().build();
     }
@@ -50,7 +52,7 @@ public class NoteController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
         Note deleteNote = noteService.deleteNoteById(id);
-        if (deleteNote == null) ResponseEntity.notFound().build();
+        if (deleteNote == null) return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/st/notesgbprac6/service/NoteService.java
+++ src/main/java/st/notesgbprac6/service/NoteService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import st.notesgbprac6.model.Note;
 import st.notesgbprac6.repository.NoteRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -13,17 +15,34 @@ public class NoteService {
     NoteRepository noteRepository;
 
     public Note saveNote(Note note) {
+        note.setDateCreated(LocalDateTime.now());
+        return noteRepository.save(note);
     }
 
     public List<Note> findAllNotes() {
+        return noteRepository.findAll();
     }
 
-    public Note getNoteById(Long id) {
+    public Optional<Note> getNoteById(Long id) {
+        return noteRepository.findById(id);
     }
 
     public Note updateNote(Long id, Note note) {
+        Optional<Note> update = noteRepository.findById(id);
+        if (update.isEmpty()) return null;
+        update.get().setHeader(note.getHeader());
+        update.get().setBody(note.getBody());
+        update.get().setDateCreated(LocalDateTime.now());
+
+        return noteRepository.save(update.get());
     }
 
     public Note deleteNoteById(Long id) {
+        Optional<Note> deleteNote = noteRepository.findById(id);
+        if (deleteNote.isEmpty()) return null;
+
+        noteRepository.delete(deleteNote.get());
+
+        return deleteNote.get();
     }
 }
