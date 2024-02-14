ebfb15388303dc3fb1374f71455db83ecc895ac8
StephYa7 <1000p@mail.ru>
2024-02-14 14:24:25 +0700

    Initial commit

---
+++ src/main/java/st/notesgbprac6/NotesGbPrac6Application.java
@@ -0,0 +1,13 @@
+package st.notesgbprac6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesGbPrac6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesGbPrac6Application.class, args);
+	}
+
+}
---
+++ src/test/java/st/notesgbprac6/NotesGbPrac6ApplicationTests.java
@@ -0,0 +1,13 @@
+package st.notesgbprac6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesGbPrac6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
